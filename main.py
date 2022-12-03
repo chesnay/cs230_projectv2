0ca9b2a3ce29bd8a3d6a3b4e06966f6492073b0a
chesnay <francois.chesnay@sunhavencapital.com>
2022-12-03 15:37:45 +0000

    amending main.py to add extra info

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ import warnings
 
 import numpy as np
 import pandas as pd
+#import tensorflow as tf
 from sklearn.metrics import roc_auc_score
+from sklearn.preprocessing import LabelEncoder
 import torch
 from torch import nn, optim
 from torch.utils.data import DataLoader
+from torch.nn.utils.rnn import pad_sequence
 from tqdm import tqdm
 from transformers import BertConfig
 
@@ -71,6 +74,113 @@ def main():
         print(train_df.columns.tolist())
         print("The column headers of questions.csv are :")
         print(question_df.columns.tolist())
+
+
+    #
+    # Compute time_lag
+    #
+    tmp_df = train_df[['user_id', 'timestamp', 'task_container_id']]
+    tmp_df = tmp_df.drop_duplicates(['user_id', 'timestamp', 'task_container_id'])
+    time_lag = np.array([0]+(tmp_df.timestamp[1:].values - tmp_df.timestamp[:-1].values).tolist(), 'float32')
+    time_lag[time_lag < 0] = 0
+    tmp_df['time_lag'] = time_lag
+    del time_lag
+    train_df = train_df.merge(tmp_df, on=['user_id', 'timestamp', 'task_container_id'], how = 'left')
+    del tmp_df
+
+    #
+    # Compute question_elapsed_time and question_had_explanation 
+    # then drop prior_question_elapsed_time, prior_question_had_explanation
+    #
+    tmp_df = train_df[['user_id', 'timestamp', 'task_container_id', 
+                                'prior_question_elapsed_time', 'prior_question_had_explanation']][train_df.content_type_id==False]
+
+    tmp_df = tmp_df.drop_duplicates(['user_id', 'timestamp', 'task_container_id'])
+
+    tmp_df['question_elapsed_time'] = tmp_df.prior_question_elapsed_time[1:].tolist()+[pd.NA]
+    tmp_df['question_had_explanation'] = tmp_df.prior_question_had_explanation[1:].tolist()+[pd.NA]
+    tmp_df = tmp_df[(tmp_df.user_id[1:].values==tmp_df.user_id[:-1].values).tolist()+[False]]
+    tmp_df = tmp_df[['user_id', 'timestamp', 'task_container_id','question_elapsed_time','question_had_explanation']]
+    train_df = train_df.merge(tmp_df, on=['user_id', 'timestamp', 'task_container_id'], how = 'left')
+    #train_df = train_df.drop(columns = ['prior_question_elapsed_time', 'prior_question_had_explanation'])
+    del tmp_df
+
+    #
+    # Compute stats on questions: question difficulty & question popularity
+    #
+    #
+    question_stats = train_df[
+                        train_df['content_type_id'] == False
+                        ].groupby(
+                            ['content_id', 'content_type_id']
+                            ).agg({
+                                'answered_correctly':'sum',
+                                'user_id':'count'
+                            }).reset_index()
+    #Rescaling                          
+    question_stats['difficulty'] = np.sqrt(1.0 - question_stats['answered_correctly']/question_stats['user_id'])
+    question_stats['popularity'] = np.power(question_stats['user_id']/question_stats['user_id'].max(), 0.25)
+    question_stats = question_stats[['content_id', 'content_type_id', 'difficulty', 'popularity']]
+
+
+####### Adding part in relation to videos##################
+
+
+
+    #
+    # Generate ids for question & lecture metadata for embeddings
+    #
+    questions = pd.read_csv(DATA_DIR + 'questions.csv')
+    lectures = pd.read_csv(DATA_DIR + 'lectures.csv')
+
+    encoded_questions = pd.DataFrame(data=questions[['question_id']].values, columns=['content_id'])
+    encoded_questions['content_type_id'] = False
+    encoded_questions['encoded_question_id'] = LabelEncoder().fit_transform(questions.question_id)
+    encoded_questions['bundle_id'] = LabelEncoder().fit_transform(questions.bundle_id)
+    encoded_questions['correct_answer'] = questions.correct_answer
+    encoded_questions['part'] = questions.part
+    tags = questions.tags.fillna('').apply(lambda x: [1+int(t) for t in str(x).split()])
+    #encoded_questions['tags'] = tf.keras.preprocessing.sequence.pad_sequences(tags).tolist()
+    print("tags:", tags)
+    for tag_test in tags:
+        print(len(tag_test))
+    exit(0)
+    encoded_questions['tags'] = pad_sequence(torch.FloatTensor(tags)).tolist()
+
+    encoded_questions  = encoded_questions.merge(question_stats, 
+                            how = 'left', 
+                            on=['content_id', 'content_type_id']).fillna(0)
+
+    encoded_lectures = pd.DataFrame(data=lectures[['lecture_id']].values, columns=['content_id'])
+    encoded_lectures['content_type_id'] = True
+
+    encoded_lectures['encoded_lecture_id'] = LabelEncoder().fit_transform(lectures.lecture_id)
+    encoded_lectures['part'] = lectures.part
+    encoded_lectures['tag'] = LabelEncoder().fit_transform(lectures.tag)
+    encoded_lectures['type_of'] = LabelEncoder().fit_transform(lectures.type_of)
+
+    # Append questions and lectures table, create new index to have
+    # a common id (encoded_content_id)
+    encoded_content = pd.DataFrame.from_dict({
+        'content_id':questions.question_id,
+        'content_type_id':len(questions)*[False]
+    }).append(
+        pd.DataFrame.from_dict({
+        'content_id':lectures.lecture_id,
+        'content_type_id':len(lectures)*[True]
+        })
+    )
+    encoded_content['encoded_content_id'] = range(len(encoded_content))
+
+    #
+    # Put the generated encoded_content_id in the train table, 
+    # 
+
+    train_df = train_df.merge(encoded_content, on=['content_id', 'content_type_id'], how = 'left')
+    train_df = train_df.drop(columns = ['content_id', 'content_type_id', 'task_container_id'])
+    train_df = train_df.astype({'question_had_explanation': 'boolean', 'encoded_content_id':'int32', 'time_lag':'float32'})
+
+
 
 
     train_df = train_df[train_df['content_type_id'] == 0].reset_index(drop=True)
